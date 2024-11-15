188bfbd27a85814c78e722ff5230027be7043a1c
Michael Zwierzanski <69387856+mz106@users.noreply.g>
2024-11-15 11:37:01 +0000

    finish lab 1

--- module_1_machine_learning_with_python/lab_1_simple_linear_regression/index.py
+++ module_1_machine_learning_with_python/lab_1_simple_linear_regression/index.py
@@ -4,6 +4,7 @@ import pylab as pl
 import numpy as np
 
 from sklearn import linear_model
+from sklearn.metrics import r2_score
 
 path = "./FuelConsumptionCo2.csv"
 
@@ -44,7 +45,7 @@ viz = cdf[['CYLINDERS', 'ENGINESIZE', "CO2EMISSIONS", 'FUELCONSUMPTION_COMB']]
 # plt.show()
 
 # Create training and test data
-# Creare mask
+# Create mask
 
 msk = np.random.rand(len(df)) < 0.8
 train = cdf[msk]
@@ -58,8 +59,34 @@ regr.fit(train_x, train_y)
 print("Coefficients: ", regr.coef_)
 print("Intercept: ", regr.intercept_) 
 
-plt.scatter(train.ENGINESIZE, train.CO2EMISSIONS, color="blue")
-plt.plot(train_x, regr.coef_[0][0] * train_x + regr.intercept_[0], "-r")
-plt.xlabel("Engine size")
-plt.ylabel("Emission")
-plt.savefig('images/train_engine_emissions_fit.png')
+# plt.scatter(train.ENGINESIZE, train.CO2EMISSIONS, color="blue")
+# plt.plot(train_x, regr.coef_[0][0] * train_x + regr.intercept_[0], "-r")
+# plt.xlabel("Engine size")
+# plt.ylabel("Emission")
+# plt.savefig('images/train_engine_emissions_fit.png')
+
+# Evaluate using engine size
+
+# test_x = np.asanyarray(test[["ENGINESIZE"]])
+test_y = np.asanyarray(test[["CO2EMISSIONS"]])
+# test_y_ = regr.predict(test_x)
+
+# print("Mean absloute error: %.2f" % np.mean(np.absolute(test_y_ - test_y)))
+# print("Residual sum of squares (MSE): %.2f" % np.mean((test_y_ - test_y) ** 2))
+# print("R2-score: %.2f" % r2_score(test_y , test_y))
+
+# Evaluate using fuel consumption
+
+train_x = np.asanyarray(train[["FUELCONSUMPTION_COMB"]])
+test_x = np.asanyarray(test[["FUELCONSUMPTION_COMB"]])
+
+regr = linear_model.LinearRegression()
+regr.fit(train_x, train_y)
+
+print("Coefficients: ", regr.coef_)
+print("Intercept: ", regr.intercept_)
+
+predictions = regr.predict(test_x)
+
+mae = np.mean(np.absolute(predictions - test_y))
+print("MAE: ", mae)
